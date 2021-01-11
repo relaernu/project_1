ff6ccea7ece6430af0a00843ea7a91dd7d0e4f51
lynyong <74089064+lynyong@users.noreply.github.com>
2021-01-11 21:30:00 +1100

    Update ideas.py

--- ideas.py
+++ ideas.py
@@ -1,2 +1,10 @@
-danceability
-danceability decade
+data: https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks
+
+questions: 
+danceability - find the most danceable decade
+duration of songs e.g. has it changed over the years? 
+which key is most popular for each decade? 
+most popular music of all times 
+
+
+
